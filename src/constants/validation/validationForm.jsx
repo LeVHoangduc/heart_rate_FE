b8b4a7ffa6584143a020031349ab9e069b36e032
LeVHoangduc <levhoangduc@gmail.com>
2023-10-11 13:16:43 +0700

    feat:update validation"

--- src/constants/validation/validationForm.jsx
+++ src/constants/validation/validationForm.jsx
@@ -4,45 +4,46 @@ import EmailValidation from "./EmailValidation"
 const ValidationLogin = (data) => {
     let fieldCheck = []
 
-    for(const key in data){
-        
-        if(key === "email"){
-            let isRegex = EmailValidation(data[key])
-            if(!isRegex){
-                const field  = {
-                    isValid: false,
-                    field:key,
-                    message:  `${key} is invalid`
-                }
-                fieldCheck.push(field)
-            }
+    const userCheck = {
+        email: data.email,
+        password: data.password,
+    }
+
+    for (const key in userCheck) {
+        const value = userCheck[key]
+
+        let isValidField = true
+        let messageRequired = ""
+        let messageInvalid = ""
+
+        if (key === "email") {
+            isValidField = EmailValidation(value)
+            messageRequired = "email required"
+            messageInvalid = "email invalid"
         }
-        if(key === "password"){
-            let isRegex = PasswordValidation(data[key])
-            if(!isRegex){
-                const field  = {
-                    isValid: false,
-                    field:key,
-                    message:  `${key} is invalid`
-                }
-                fieldCheck.push(field)
-            }
-        }
-        if(data[key] === " "){
-            const field  = {
-                isValid: false,
-                field:key,
-                message:  `${key} is required`
-            }
-            fieldCheck.push(field)
+        if (key === "password") {
+            isValidField = PasswordValidation(value)
+            messageRequired = "password required"
+            messageInvalid = "password invalid"
         }
 
-        else{
-            const field  = {
+        if (value.trim() === "") {
+            fieldCheck.push({
+                field: key,
+                isValid: false,
+                message: messageRequired,
+            })
+        } else if (!isValidField) {
+            fieldCheck.push({
+                field: key,
+                isValid: false,
+                message: messageInvalid,
+            })
+        } else {
+            fieldCheck.push({
+                field: key,
                 isValid: true,
-                field:key,
-            }
-            fieldCheck.push(field)
+            })
         }
     }
 

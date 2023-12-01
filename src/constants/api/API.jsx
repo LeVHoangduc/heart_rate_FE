846a8ef29399e23046732ae0306d3ee480fd406d
LeVHoangduc <levhoangduc@gmail.com>
2023-12-01 22:09:53 +0700

    feat:update path url constants

--- src/constants/api/API.jsx
+++ src/constants/api/API.jsx
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios from 'axios'
+import { PATH_URL } from '../values'
+
 export default axios.create({
-    baseURL: "http://127.0.0.1:8000/api/"
+  baseURL: PATH_URL,
 })
